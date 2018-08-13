dacd31fe8c4e60d9347ecf68197b7e54b55009d1
Nyandika Donald <donald@pixsel.net>
2018-08-13 15:58:34 +0300

    Add Project library for .Net Core 2.* versions

---
+++ Example.NetCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using EncryptionCore;
+using System;
+
+namespace Example.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize the encryption class
+            TarajiEncryption encryption = new TarajiEncryption();
+            var cryptographer = encryption.MakeCryptographer();
+
+            string encryptedString = cryptographer.Encrypt("stringorpasswordtoencrypt");
+            string plainString = cryptographer.Decrypt("IwUXysKfykPHGBOXUEiPwKzim1CUz53m26JDZGLghBs=");
+
+            Console.WriteLine(encryptedString);
+            Console.WriteLine(plainString);
+            Console.ReadLine();
+        }
+    }
+}
